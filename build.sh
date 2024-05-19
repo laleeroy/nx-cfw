2b5ca261963777a07cfda0265ef1e74c40c5e66c
laleeroy <gabwielcastwo@gmail.com>
2024-05-19 10:18:27 +0800

    Update ams links
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build.sh
+++ build.sh
@@ -16,8 +16,9 @@ prepare_hekate() {
 }
 
 prepare_atmosphere() {
-    curl -O -L https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.7.0-prerelease/atmosphere-1.7.0-master-35d93a7c4+hbl-2.4.4+hbmenu-3.6.0.zip --output-dir $TMP_DIR
-    $UNZIP_COMMAND $TMP_DIR/atmosphere-*.zip -d $BUILD_DIR
+	curl -H "Authorization: Bearer $GITHUB_TOKEN" -O -L https://github.com/laleeroy/Atmosphere-/releases/download/1/atmosphere-1.7.0-master-6d62a2439-hbl-+hbmenu-.zip --output-dir $TMP_DIR
+	ls $TMP_DIR
+	$UNZIP_COMMAND $TMP_DIR/atmosphere-*.zip -d $BUILD_DIR
 }
 
 prepare_overlays() {
